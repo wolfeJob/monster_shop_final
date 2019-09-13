b5a8e49e198c225c1dc435cd4fc7a386f2764f57
Scott Payton <spayton.c@gmail.com>
2019-09-13 10:01:05 -0600

    Created migrations andaddresses table

---
+++ spec/features/user/addresses/new_spec.rb
@@ -0,0 +1,9 @@
+# require 'rails_helper'
+#
+# describe 'Creating a new address for a registered user' do
+#  before(:each)
+#  @scott = User.create!(name: 'Scott', email: 'scott@gmail.com', password: 'apple')
+#  @scott_address = @scott.addresses.create!(street: '111 Willow St', city: 'Denver', state: 'CO', zip: 99999, nickname: "home")
+#
+#    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+#   end
