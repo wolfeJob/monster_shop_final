6edfee90271a0a386adfc0df74dcdaa3798ba909
Scott Payton <spayton.c@gmail.com>
2019-09-13 12:23:26 -0600

    fixed user creation in spec

--- spec/features/user/addresses/new_spec.rb
+++ spec/features/user/addresses/new_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'Creating a new address for a registered user' do
  before(:each) do
- @scott = User.create!(name: 'Scott', email: 'scott@gmail.com', password: 'apple')
- @scott_address = @scott.addresses.create!(street: '111 Willow St', city: 'Denver', state: 'CO', zip: 99999, nickname: "home")
+ @scott = User.create!(name: 'Scott', email: 'scotto@gmail.com', password: 'apple')
+ # binding.pry
+ @scott_addy = @scott.addresses.create!(street: '111 Willow St', city: 'Denver', state: 'CO', zip: 99999, nickname: "home")
  allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@scott)
   end
 
