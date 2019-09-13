e8d786db0cfc69bf9fb23a4f04f3432857b61f66
Scott Payton <spayton.c@gmail.com>
2019-09-13 12:12:57 -0600

    fixed migration users to addresses

--- db/migrate/20190913154643_add_addresses_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressesToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :addresses, :users, foreign_key: true
-  end
-end
--- db/migrate/20190913155700_add_addresses_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressesToOrders < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :orders, :addresses, foreign_key: true
-  end
-end
